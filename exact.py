ce3c4b9cd46f7fd31655880ca6fdc1ebb921913b
zylatis <graeme.gossel@gmail.com>
2019-01-06 19:31:18 +0800

    Working 2electron code for 2 nuclear centres - gives correct GS and first few ex states for helium as well. Had bug in loop giving rubbish eigenvectors for plots but fixed now. Next task: loop over to generate density<->energy pairs to start on ML part

--- exact.py
+++ exact.py
@@ -1,30 +1,55 @@
 from __future__ import division #something about integer division
 import numpy as np
 from scipy.sparse import diags, bmat
-from scipy.sparse.linalg import eigsh
+from scipy.sparse.linalg import eigsh, eigs
 import matplotlib.pyplot as plt
-from sklearn.neural_network import MLPRegressor
+from operator import add
 
-L = 1.
-N = 200
+def make_potential( R1,R2, A1, A2 ):
+	potential = []
+	for i in range(N):
+		for j in range(N):
+			r1 = -L/2. + i*dx
+			r2 = -L/2. + j*dx
+			
+			c1r1  = -A1/np.sqrt(1.+(r1-R1)**2)
+			c1r2  = -A1/np.sqrt(1.+(r2-R1)**2)
+			
+			c2r1  = -A2/np.sqrt(1.+(r1-R2)**2)
+			c2r2  = -A2/np.sqrt(1.+(r2-R2)**2)
+			
+			v12 = 1./np.sqrt(1.+(r1-r2)**2)
+			
+			#~ v1  = r2**2 + r1**2
+			#~ v2 = 0
+			#~ v12 = 0
+			potential.append( c1r1 + c1r2 + c2r1 + c2r2  + v12 )
+	return diags( potential, shape = (N**2,N**2) ) 
+	
+def comp_gs(R1, R2, A1, A2, n ):
+	potential = make_potential(R1, R2,A1,A2)
+# which='SA', k = n_levels, tol = 0,ncv = 250
+	vals, vecs = eigsh(-0.5*H + potential, k = n, which = 'SA', return_eigenvectors = True) #want smallest algebraic not magnitude, need bound state
+	vecsE = vecs
+	vecs = [ x[n-1] for x in vecs ]
+	
+	density = [0]*N
+	for i in range(N):
+		density = list( map(add, density,vecs[N*i:(i+1)*N] ) ) # convert to density by integrating out one part of flat 2D vector
+
+	for i in range(N):
+		density[i] = np.abs(density[i])**2
+	norm = sum(density)*dx
+	density = density/norm
+	#~ print density[int(N/2)]
+	#~ print vecs[int(N/2)]
+	return [ vals, density, vecsE ]
+	
+	
+L = 50
+N = 300
 dx = L/(N-1.)
-#~ x = np.linspace(0,L,N)
-#~ H = diags([1., -2., 1.], [-1,0, 1], shape=(N, N))/dx**2
-
-#~ potential = [ -1./np.sqrt((dx*(r-N/2))**2 + 0.1) for r in range(0, N) ]
-#~ V = diags(potential, 0,  shape=(N, N))
-
-#~ n_levels = 1
-#~ vals, vecs = eigsh(-0.5*H , which='SA', k = n_levels) #want smallest algebraic not magnitude, need bound state
-#~ print vals
-#~ exit(0)
-#~ print vals
-#~ plt.plot(x,vecs[:,0])
-#~ plt.plot(x,vecs[:,1])
-
-#~ plt.plot(x,potential)
-#~ plt.show()
-
+x_points = np.linspace(-L/2,L/2,N)
 
 B  = diags([1., -4., 1.], [-1,0, 1], shape=(N, N))
 I = diags(1., shape=(N, N))
@@ -33,26 +58,21 @@ I = diags(1., shape=(N, N))
 H = bmat([[B if i == j else I if abs(i-j)==1
                 else None for i in range(N)]
                 for j in range(N)], format='bsr')/dx**2
+                
+R1 = 5
+R2 = -5
+A1 = 1
+A2 = 1
 
-potential = []
-for i in range(N):
-	for j in range(N):
-		r1 = -L/2. + i*dx
-		r2 = -L/2. + j*dx
-		v1  = -2./np.sqrt(1.+r1**2)
-		v2  = -2./np.sqrt(1.+r2**2)
-		v12 = 1./np.sqrt(1.+(r1-r2)**2)
-		
-		#~ v1  = r2**2 + r1**2
-		#~ v2 = 0
-		#~ v12 = 0
-		potential.append( v1 + v2 + v12 )
-		
-potential = diags( potential, shape = (N**2,N**2) ) 
+E, nd, vecsE = comp_gs(R1,R2, A1, A2, 3)
+print E
+out_nd = np.asarray(nd)
+i = 0;
+np.savetxt("train_data/" + str(i) + ".dat", out_nd)
 
-n_levels = 8
-vals, vecs = eigsh(-0.5*H + potential, which='SA', k = n_levels, tol = 0) #want smallest algebraic not magnitude, need bound state
-print vals
-#~ print vals[3]-vals[2]
-#~ print vals[2]-vals[1]
-#~ print vals[1]-vals[0]
+V1 = [ -A1/np.sqrt(1.+( -L/2. + i*dx-R1)**2) + -A2/np.sqrt(1.+( -L/2. + i*dx-R2)**2) for i in range(N) ]
+plt.plot(x_points, V1)
+plt.plot(x_points, nd)
+
+#~ plt.show()
+plt.savefig('temp.png')
