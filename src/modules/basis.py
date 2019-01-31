aa46808d71de6ed96c2ca7dcc9d94ac55959bdc7
zylatis <graeme.gossel@gmail.com>
2019-02-01 10:27:05 +1100

    Oops, folder git commit fail - now basis code updated

--- src/modules/basis.py
+++ src/modules/basis.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from scipy.optimize import minimize, curve_fit
+from scipy.optimize import minimize, curve_fit, basinhopping
 from scipy.signal import find_peaks
 # un-normalised gaussians
 def gaussian( x, pars ):
@@ -40,3 +40,24 @@ def gaussian_exp( x , y, basis_size, Arange = [0.1,20],crange = [0,250],sigmaran
 	upper_bound = sum( [ [ Arange[1],crange[1],sigmarange[1]] for n in range(basis_size)], [])
 	popt,pcov = curve_fit( model, x , y, p0 = p0,  maxfev = 10000000, bounds = (lower_bound, upper_bound))
 	return popt
+
+
+def cost_fn( x0, *params ):
+	x_grid = params[0]
+	y_actual = params[1]
+	y_pred = model( x_grid, *x0 )
+	cost = np.sum((y_pred - y_actual)**2)
+	return cost
+
+
+def test(x, y_actual, basis_size):
+	params = (x,y_actual)
+	# p0 = [10.]*12
+	peaks, peak_heights = find_peaks(y_actual, prominence = 0.01)
+	p0_0 = sum( [ [ 0.1 , peaks[n], 10 ] for n in range(len(peaks))], [])
+	
+	if len(peaks)<basis_size:
+		p0_1 = sum( [ [0.1 , np.mean(peaks), 1] for n in range(basis_size - len(peaks))], [])
+	p0 = p0_0 + p0_1
+	fit = basinhopping(cost_fn, p0, minimizer_kwargs={"args" : params})
+	return fit['x'], len(peaks)
